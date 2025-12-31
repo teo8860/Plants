cfa9e42048a5fc3e1c854510c173f2547a471200
MrSimsure <simonebenigni.dev@gmail.com>
2025-12-31 11:55:56 +0100

    sync

---
+++ Engine/GameGeneral/Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plants;
+
+public class Room
+{
+	static List<Room> rooms = new List<Room>();
+
+	public Room()
+	{
+		Room.rooms.Add(this);
+	}
+}
